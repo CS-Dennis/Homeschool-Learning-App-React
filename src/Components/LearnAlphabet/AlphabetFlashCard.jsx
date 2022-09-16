dac2c990dfe6e271568f1ea75aaaa2a632ad101d
Dennis <dennis@test.com>
2022-09-15 23:59:09 -0500

    show all alphabet cards feature added

--- src/Components/LearnAlphabet/AlphabetFlashCard.jsx
+++ src/Components/LearnAlphabet/AlphabetFlashCard.jsx
@@ -1,12 +1,12 @@
 import { Paper } from "@mui/material";
 import React from "react";
 
-export default function AlphabetFlashCard({ letter }) {
+export default function AlphabetFlashCard({ letter, fontSize, imageWidth }) {
   return (
     <>
       <Paper elevation={3} sx={{textAlign: 'center'}}>
-        <div style={{ fontSize: "20em", textAlign: "center" }}>{letter}</div>
-        <img src={"./imgs/alphabets/"+letter.toLowerCase()+".png"} alt="image" width="200px" />
+        <div style={{ fontSize: fontSize, textAlign: "center" }}>{letter}</div>
+        <img src={"./imgs/alphabets/"+letter.toLowerCase()+".png"} alt="image" width={imageWidth} />
       </Paper>
     </>
   );

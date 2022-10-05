813d1c7986b11dcd22339c3c0ff2fe87e58f4570
Dennis <dennis@test.com>
2022-10-04 22:47:25 -0500

    maze game letter position fixed

--- src/Components/LearnAlphabet/MazeGame.jsx
+++ src/Components/LearnAlphabet/MazeGame.jsx
@@ -12,6 +12,7 @@ import { Link } from "react-router-dom";
 import Header from "../Header";
 import KeyboardBackspaceRoundedIcon from "@mui/icons-material/KeyboardBackspaceRounded";
 import { randomNumList } from "../util";
+import { VerticalAlignCenter } from "@mui/icons-material";
 
 export default function MazeGame() {
   const maze1 = {
@@ -32,7 +33,7 @@ export default function MazeGame() {
       [0, 0],
       [9, 9],
     ],
-    logos:["tl", "bl"]
+    logos: ["tl", "bl"],
   };
 
   const maze2 = {
@@ -53,7 +54,7 @@ export default function MazeGame() {
       [0, 3],
       [9, 9],
     ],
-    logos:["tl", "bl"]
+    logos: ["tl", "bl"],
   };
 
   const maze3 = {
@@ -74,7 +75,7 @@ export default function MazeGame() {
       [0, 6],
       [9, 1],
     ],
-    logos:["tl", "bl"]
+    logos: ["tl", "bl"],
   };
 
   const maze4 = {
@@ -95,7 +96,7 @@ export default function MazeGame() {
       [2, 0],
       [0, 6],
     ],
-    logos:["l", "tl"]
+    logos: ["l", "tl"],
   };
 
   const maze5 = {
@@ -116,7 +117,7 @@ export default function MazeGame() {
       [5, 0],
       [9, 7],
     ],
-    logos:["l", "bl"]
+    logos: ["l", "bl"],
   };
 
   const maze6 = {
@@ -137,7 +138,7 @@ export default function MazeGame() {
       [9, 0],
       [0, 7],
     ],
-    logos:["l", "tl"]
+    logos: ["l", "tl"],
   };
 
   const maze7 = {
@@ -158,7 +159,7 @@ export default function MazeGame() {
       [9, 1],
       [0, 6],
     ],
-    logos:["bl", "tl"]
+    logos: ["bl", "tl"],
   };
 
   const maze8 = {
@@ -179,7 +180,7 @@ export default function MazeGame() {
       [0, 3],
       [9, 4],
     ],
-    logos:["tl", "bl"]
+    logos: ["tl", "bl"],
   };
 
   const maze9 = {
@@ -200,7 +201,7 @@ export default function MazeGame() {
       [0, 7],
       [9, 7],
     ],
-    logos:["tl", "bl"]
+    logos: ["tl", "bl"],
   };
 
   const mazes = [maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8, maze9];
@@ -289,7 +290,7 @@ export default function MazeGame() {
           setTimeout(() => {
             setCongrats(false);
             initGame();
-          }, 1000);
+          }, 1500);
         }
       }
     }
@@ -367,7 +368,7 @@ export default function MazeGame() {
           </Link>
         </Grid>
       </Grid>
-
+      
       <Grid
         container
         sx={{
@@ -426,14 +427,24 @@ export default function MazeGame() {
         </Grid>
       </Grid>
 
-      <Grid container sx={{width: '80%', margin:'auto', left:0, right:0, marginTop: "50px", marginBottom: "50px"}}>
+      <Grid
+        container
+        sx={{
+          width: "80%",
+          margin: "auto",
+          left: 0,
+          right: 0,
+          marginTop: "50px",
+          marginBottom: "50px",
+        }}
+      >
         <Grid item md={2} lg={3}></Grid>
         <Grid
           item
           xs={12}
           md={8}
           lg={6}
-          sx={{ marginTop: "10px", marginBottom: "10px", cursor: "pointer",  }}
+          sx={{ marginTop: "10px", marginBottom: "10px", cursor: "pointer" }}
         >
           {currentMaze.maze.map((array, row) => (
             <Grid container>
@@ -441,15 +452,42 @@ export default function MazeGame() {
                 <Grid
                   item
                   xs
-                  sx={{ display: "block", position: 'relative' }}
+                  sx={{ display: "block", position: "relative" }}
                   onClick={() => checkTile(num, row, col)}
                 >
                   {/* start point logo */}
-                  {(currentMaze.points[0][0] === row && currentMaze.points[0][1] === col)
-                    && (<img src="./imgs/dog17.png" alt="dog" width="80px" style={{position: "absolute", left :(currentMaze.logos[0]==='l'?"-65px":"-30px"), top:(currentMaze.logos[0]==='tl' && '-75px'), bottom: (currentMaze.logos[0]==="bl" && '-75px'), index: -10}} />)}
+                  {currentMaze.points[0][0] === row &&
+                    currentMaze.points[0][1] === col && (
+                      <img
+                        src="./imgs/dog17.png"
+                        alt="dog"
+                        width="80px"
+                        style={{
+                          position: "absolute",
+                          left:
+                            currentMaze.logos[0] === "l" ? "-65px" : "-30px",
+                          top: currentMaze.logos[0] === "tl" && "-75px",
+                          bottom: currentMaze.logos[0] === "bl" && "-75px",
+                          index: -10,
+                        }}
+                      />
+                    )}
                   {/* end point logo */}
-                  {(currentMaze.points[1][0] === row && currentMaze.points[1][1] === col)
-                    && (<img src="./imgs/bowl.png" alt="bowl" width="70px" style={{position: "absolute", left :"-30px", top:(currentMaze.logos[1]==='tl' && '-60px'), bottom: (currentMaze.logos[1]==="bl" && '-60px'), index: -10}} />)}
+                  {currentMaze.points[1][0] === row &&
+                    currentMaze.points[1][1] === col && (
+                      <img
+                        src="./imgs/bowl.png"
+                        alt="bowl"
+                        width="70px"
+                        style={{
+                          position: "absolute",
+                          left: "-30px",
+                          top: currentMaze.logos[1] === "tl" && "-60px",
+                          bottom: currentMaze.logos[1] === "bl" && "-60px",
+                          index: -10,
+                        }}
+                      />
+                    )}
                   <Box
                     className={
                       "mazeTile " +
@@ -468,16 +506,21 @@ export default function MazeGame() {
                   >
                     <span
                       style={{
-                        margin: "auto",
-                        left: 0,
-                        right: 0,
-                        top: 0,
-                        bottom: 0,
-                        fontSize: "5vw",
-                        width: "5vw",
-                        height: "5vw",
-                        position: "absolute",
-                        textAlign: "center",
+                        
+                        fontSize: "4vmax",
+                        // width: "100%",
+                        height: "100%",
+                        width: "100%",
+                        position: 'absolute',
+                        margin: 'auto',
+                        top:'50%',
+                        // bottom:0,
+                        transform: 'translateY(-50%)',
+                        left:0,
+                        right:0,
+                        // backgroundColor: "red",
+                        textAlign: 'center'
+                        
                       }}
                     >
                       {/* if num==1, show the current letter; otherwise, show random letters */}
@@ -515,6 +558,7 @@ export default function MazeGame() {
               right: 0,
               top: 0,
               bottom: 0,
+              zIndex: 100
             }}
           />
         </Box>
@@ -535,6 +579,7 @@ export default function MazeGame() {
               right: 0,
               top: 0,
               bottom: 0,
+              zIndex: 100
             }}
           />
         </Box>
